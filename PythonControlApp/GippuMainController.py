d37bad0578fd3fb8c1ef29158ac078533ab2502c
BogdanTNT <bogdan.rosca.drago@gmail.com>
2023-08-30 15:29:35 +0300

    Added drag and drop on the position of the robot

--- PythonControlApp/GippuMainController.py
+++ PythonControlApp/GippuMainController.py
@@ -56,7 +56,29 @@ def AddPosition():
     Robot.AddPosition(-1)
     redirect(url_for('teachPanel'))
 
+# @app.route('/remove/<item>', methods=['POST'])
+# def remove_item(item):
+#     Robot.program.pop(int(item) - 1)
 
+#     # if item in Robot.program:
+#     #     print("da")
+#     return redirect(url_for('teachPanel'))
+
+@app.route('/remove/<item_index>', methods=['POST'])
+def remove_item(item_index):
+    index = int(item_index) - 1
+    if 0 <= index < len(Robot.program):
+        Robot.program.pop(index)
+    return redirect(url_for('teachPanel'))
+
+@app.route('/reorder/<from_index>/<to_index>', methods=['POST'])
+def reorder_items(from_index, to_index):
+    from_index = int(from_index)
+    to_index = int(to_index)
+    if 0 <= from_index < len(Robot.program) and 0 <= to_index < len(Robot.program):
+        element = Robot.program.pop(from_index)
+        Robot.program.insert(to_index, element)
+    return redirect(url_for('teachPanel'))
 
 # @app.route('/update_element', methods=['POST'])
 # def update_element():
